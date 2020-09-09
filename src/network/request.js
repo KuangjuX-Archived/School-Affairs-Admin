cd527340820c92314c411e2353d80147958ba849
KuangjuX <18630816527@163.com>
2020-09-09 23:11:28 +0800

    initialization

---
+++ src/network/request.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+export function request(config) {
+    const instance = axios.create({
+        baseURL: "",
+        timeout: 50000
+    })
+
+    return instance(config)
+}
---
+++ src/views/Login.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <v-container style="margin-top: 5%">
+            <v-row>
+                <v-col cols="3"></v-col>
+                <v-col cols="6">
+                    <v-card>
+                        <v-card-title>
+                            <div class="card-title-element">登录</div>
+                        </v-card-title>
+                        <v-card-text>
+                            <div style="margin: 10%">
+                                <v-form
+                                        lazy-validation
+                                        ref="form"
+                                >
+                                    <v-text-field
+                                            v-model="form.username"
+                                            label="用户名"
+                                            required
+                                    ></v-text-field>
+
+                                    <v-text-field
+                                            v-model="form.password"
+                                            label="密码"
+                                            type="password"
+                                            required
+                                    ></v-text-field>
+
+                                    <div>
+                                        <v-btn v-if="isSubmit" @click="login" rounded block  dark>登录</v-btn>
+                                        <v-btn v-else   rounded block disabled>登录</v-btn>
+                                    </div>
+
+
+
+
+
+                                    <v-divider></v-divider>
+
+                                    <div style="text-align: center">
+                                        <a @click="$router.push(`/register`)">没有账号？赶紧去注册！</a>
+                                    </div>
+
+                                </v-form>
+                            </div>
+                        </v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col cols="3"></v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data() {
+            return {
+                form: {
+                    username: "",
+                    password: ""
+                },
+            }
+        },
+
+        methods: {
+          login(){
+
+          }
+        },
+
+        computed: {
+            isSubmit() {
+                for (let item in this.form){
+                    let arri=this.form[item];
+                    if(arri===undefined||arri===""||arri==null){
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+    a{
+        text-align: center;
+    }
+
+    a:hover{
+        cursor: pointer;
+        color: #007bff;
+    }
+
+
+    .card-title-element{
+        align-items: center;
+        margin-left: 45%;
+        font-weight: 900;
+        font-size: 25px;
+    }
+</style>
