07b6e1311e6cf8144710b48f86483729b772aa0e
KuangjuX <18630816527@163.com>
2020-09-11 13:16:30 +0800

    add profile card

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,8 +1,13 @@
 import Cookies from 'js-cookie'
-export function setUserInfo(token, id, phoneNumber) {
-    Cookies.set('userToken', token);
-    Cookies.set('userId', id);
-    Cookies.set('phoneNumber',phoneNumber);
+export function setUserInfo(token, id) {
+    //过期时间: 2h
+    var expiresTime = new Date(millisecond + 60 * 1000 * 120);
+    Cookies.set('userToken', token, {
+        expires: expiresTime
+    });
+    Cookies.set('userId', id, {
+        expires: expiresTime
+    });
 }
 export function deleteToken() {
     Cookies.remove('userToken');
