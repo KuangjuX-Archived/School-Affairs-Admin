4014d8a7f38fa62532bb1ba026ca0d7eb8ca34d8
v25bh145 <1368179767@qq.com>
2020-09-11 12:27:07 +0800

    过期时间 2H

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -1,7 +1,13 @@
 import Cookies from 'js-cookie'
 export function setUserInfo(token, id) {
-    Cookies.set('userToken', token);
-    Cookies.set('userId', id);
+    //过期时间: 2h
+    var expiresTime = new Date(millisecond + 60 * 1000 * 120);
+    Cookies.set('userToken', token, {
+        expires: expiresTime
+    });
+    Cookies.set('userId', id, {
+        expires: expiresTime
+    });
 }
 export function deleteToken() {
     Cookies.remove('userToken');
